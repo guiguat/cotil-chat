7dd856f8fa6deb7c6a0fd940ad7695520532ee87
iuri-pdista <correaiuri86@gmail.com>
2020-07-12 23:23:29 -0300

    Create Join

--- client/src/Pages/Join/index.tsx
+++ client/src/Pages/Join/index.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import Navbar from '../../Components/Navbar';
 
 const Join: React.FC = () => {
-  return <h1>ello Join</h1>;
+  return (
+  <>                                                
+    <div className="content">
+      <Navbar>
+        <h1 className="nav-title">
+          COTIL CHAT       
+        </h1>
+      </Navbar>
+      <h2>Entre na sala:</h2>
+      <form className="join-form">  
+          <label htmlFor="name">Nome:</label>
+          <input id="name"/>
+          <label htmlFor="room">Código da sala:</label>
+          <input id="room"/>
+          <button type="submit" className="btn-join">Entrar</button>
+      </form>  
+    </div>
+  </>
+  );
 }
 
 export default Join;
