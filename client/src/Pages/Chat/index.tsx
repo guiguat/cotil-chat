5b82f28baab6adbf1ae4820a9ad3128002104bcc
iuri-pdista <correaiuri86@gmail.com>
2020-07-12 21:03:57 -0300

    Create Pages

---
+++ client/src/Pages/Chat/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Chat: React.FC = () => {
+  return <h1>Hello Chato</h1>;
+}
+
+export default Chat;
---
+++ client/src/Pages/Join/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Join: React.FC = () => {
+  return <h1>ello Join</h1>;
+}
+
+export default Join;
