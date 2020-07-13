1290c226a9bc8fdbb8fb3ade446dfb7309c991a1
Guilherme Guatura <guaturadzn@gmail.com>
2020-07-13 08:13:16 -0300

    provide context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { MsgProvider } from './Contexts/msg';
 import Routes from './Routes/routes';
 
 const App: React.FC = () => {
   return (
-    <Routes />
+    <MsgProvider>
+      <Routes />
+    </MsgProvider>
   );
 }
 
