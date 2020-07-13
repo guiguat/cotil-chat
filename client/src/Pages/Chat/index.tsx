34e9c70e3efbef06b066ce438e1385887cc30f8a
Guilherme Guatura <guaturadzn@gmail.com>
2020-07-13 07:01:58 -0300

    create chat nav and background

--- client/src/Pages/Chat/index.tsx
+++ client/src/Pages/Chat/index.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
+import { FiArrowLeft } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import Navbar from '../../Components/Navbar';
 
 const Chat: React.FC = () => {
   return (
     <>
-      <Navbar>
-        <h1 className="nav-title">
-          COTIL CHAT       
-        </h1>
-      </Navbar>
-      <h1>
-        hello CHAT
-      </h1>
+      <div className="content">      
+        <Navbar>
+          <div className="nav-links">
+            <Link to="/" id="arrow-back">
+              <FiArrowLeft size={24} />
+            </Link>
+            <h1 className="nav-title">
+              Sala: 291290xf      
+            </h1>
+          </div>
+        </Navbar>
+        <footer>
+          <p>
+            <a href="https://github.com/guiguat/simple-chat" target="_blank" rel="noopener noreferrer"> 
+              Made with ♥ by <br className="mobile"/> 
+              Guilherme Guatura
+              & Iuri Corrêa
+            </a>
+          </p>
+        </footer> 
+      </div>
     </>
   );
 }
