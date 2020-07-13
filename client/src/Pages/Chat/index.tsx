947724f959c7fc49a5a9d7214019ec2318ef1dcf
iuri-pdista <correaiuri86@gmail.com>
2020-07-12 23:22:56 -0300

    Add navbar, and create UI

--- client/src/Pages/Chat/index.tsx
+++ client/src/Pages/Chat/index.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import Navbar from '../../Components/Navbar';
 
 const Chat: React.FC = () => {
-  return <h1>Hello Chato</h1>;
+  return (
+    <>
+      <Navbar>
+        <h1 className="nav-title">
+          COTIL CHAT       
+        </h1>
+      </Navbar>
+      <h1>
+        hello CHAT
+      </h1>
+    </>
+  );
 }
 
 export default Chat;
