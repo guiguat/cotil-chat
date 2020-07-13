c3352a2c7533546329aaeb99e62e3553b1740d3a
Guilherme Guatura <guaturadzn@gmail.com>
2020-07-13 08:13:50 -0300

    create ui

--- client/src/Pages/Chat/index.tsx
+++ client/src/Pages/Chat/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { FiArrowLeft } from 'react-icons/fi';
+import { MdSend } from 'react-icons/md';
 import { Link } from 'react-router-dom';
 import Navbar from '../../Components/Navbar';
+import './style.css';
 
 const Chat: React.FC = () => {
   return (
@@ -17,6 +19,24 @@ const Chat: React.FC = () => {
             </h1>
           </div>
         </Navbar>
+        <div id="interface">
+          <div id="interface-content">
+            <div id="chat">
+              <div className="msg-body">
+                <span className="msg-name">Iurir</span>
+                <p className="msg-content">
+                  Eae gostosa
+                </p>
+              </div>
+            </div>
+            <form id="msg-form">
+              <input type="text" className="input"/>
+              <button id="btn-send">
+                <MdSend size={24} color="#DEDEDE"/>
+              </button>
+            </form> 
+          </div>
+        </div>
         <footer>
           <p>
             <a href="https://github.com/guiguat/simple-chat" target="_blank" rel="noopener noreferrer"> 
