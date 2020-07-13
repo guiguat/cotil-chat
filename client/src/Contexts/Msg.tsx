9c3ae161326e80b009a78b433aeb49627c9704dd
Guilherme Guatura <guaturadzn@gmail.com>
2020-07-13 07:28:11 -0300

    create msg context

---
+++ client/src/Contexts/Msg.tsx
@@ -0,0 +1,21 @@
+import React, {useState, useContext} from 'react';
+
+interface IMsgContext{
+    name:string;
+    room:string;
+    setName(name:string):void;
+    setRoom(room:string):void
+}
+export const MsgContext = React.createContext<IMsgContext>({} as IMsgContext);
+
+export const MsgProvider: React.FC = ({children}) => {
+    const[name, setName] = useState("");
+    const[room, setRoom] = useState("");
+    return (
+        <MsgContext.Provider value={ { name, room, setName, setRoom } }>
+        {children}
+        </MsgContext.Provider>
+    );
+}
+
+export const useMsg = () => useContext(MsgContext);
