2fc7f92d22842181892f32832175e95561fb526d
Guilherme Guatura <guaturadzn@gmail.com>
2020-07-14 19:04:30 -0300

    add build

---
+++ build/precache-manifest.66c6de1eced5969906f1c108fcfc25a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e633173c3275da8b94e0a7d918e1555e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e46d7188a0ed6bfca4b8",
+    "url": "./static/css/main.e2e3f20f.chunk.css"
+  },
+  {
+    "revision": "4f83641d9fdea6ca82e9",
+    "url": "./static/js/2.60596ba4.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "./static/js/2.60596ba4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e46d7188a0ed6bfca4b8",
+    "url": "./static/js/main.b603faa5.chunk.js"
+  },
+  {
+    "revision": "a4530cbc0274ace1abaa",
+    "url": "./static/js/runtime-main.5583eba1.js"
+  },
+  {
+    "revision": "ab94bd9e6060f60f000a45874cba62ae",
+    "url": "./static/media/unicamp.ab94bd9e.png"
+  }
+]);
